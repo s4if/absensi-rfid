d9c9e30d6a9e3013b2166bd7f1cd1c06db929e8a
Saiful Habib <sangpejuang32@gmail.com>
2022-12-19 22:25:01 +0700

    bootstrap untuk siswa

---
+++ app/Database/Migrations/2022-12-17-034055_Student.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class Student extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nis' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => 24,
+                'unique'        => true,
+            ],
+            'name' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => 60,
+                'null'          => false,
+            ],
+            'gender' => [
+                'type'          => 'ENUM',
+                'constraint'    => ['L', 'P'],
+                'null'          => false
+            ],
+            'classroom' => [ // nama kelas (biar gak konflik dengan class)
+                'type'          => 'ENUM',
+                'constraint'    => ['TKJ1', 'TKJ2'],
+                'null'          => false,
+            ],
+            'created_at' => [
+                'type'          => 'TIMESTAMP',
+                'default'       => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type'          => 'TIMESTAMP',
+                'null'          => true,
+            ],
+            'deleted_at' => [
+                'type'          => 'TIMESTAMP',
+                'null'          => true,
+            ],
+
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('students');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('students');
+    }
+}
