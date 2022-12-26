4a4c10ad0164f551996c24eb729b4bc692f2e402
Saiful Habib <sangpejuang32@gmail.com>
2022-12-26 13:00:28 +0700

    script untuk reset database

---
+++ reset_db.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm writable/db.sqlite && touch writable/db.sqlite
+php spark migrate
+php spark db:seed Admin
+php spark db:seed Student
+php spark db:seed DeviceRfid
+php spark db:seed Session
