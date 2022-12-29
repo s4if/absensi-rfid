f327352e732649a3bbd24e2648b6b11247be99fd
Saiful Habib <sangpejuang32@gmail.com>
2022-12-29 11:18:00 +0700

    add prod device

--- app/Database/Seeds/DeviceRfid.php
+++ app/Database/Seeds/DeviceRfid.php
@@ -9,12 +9,31 @@ class DeviceRfid extends Seeder
     public function run()
     {
         $data = [
-            'id'    => 'W001',
-            'name'  => 'Web Dummy',
-            'token' => 'RAVEN'
+            [
+                'id'    => 'W001',
+                'name'  => 'Web Dummy',
+                'token' => 'RAVEN'
+            ],
+            [
+                'id'    => 'D001',
+                'name'  => 'NodeMCU dev device',
+                'token' => 'RABBIT'
+            ],
+            [
+                'id'    => 'D002',
+                'name'  => 'Lolin dari adjie',
+                'token' => 'RACOON'
+            ],
+            [
+                'id'    => 'D003',
+                'name'  => 'ESP32 perangkat baru',
+                'token' => 'LYCAN'
+            ],
         ];
-        $this->db->table('devices')->insert($data);
-        // tempat simpan sementara rfid, ada current dan last
+        foreach ($data as $item) {
+            $this->db->table('devices')->insert($item);
+        }
+        // tempat simpan sementara rfid, hanya current
         $this->db->table('rfid_tmp')->insert(['id' => 'CURRENT']);
     }
 }
