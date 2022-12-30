f2ec3b5ff0fbd52e642a1ef5870d3043dde69369
Saiful Habib <sangpejuang32@gmail.com>
2022-12-30 13:36:34 +0700

    hapus resetdb

--- reset_db.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm writable/db.sqlite && touch writable/db.sqlite
-php spark migrate
-php spark db:seed Admin
-#php spark db:seed Student
-php spark db:seed DeviceRfid
-php spark db:seed Session
